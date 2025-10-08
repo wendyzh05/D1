b9403db8f8840db29a0904c6b597de89f06d7eea
Wendy Zhang <156264212+wendyzh05@users.noreply.github.com>
2025-10-08 18:27:54 +0000

    Add counter (Step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,11 @@ document.body.append(container);
 
 const counterDiv: HTMLDivElement = document.createElement("div");
 counterDiv.id = "counterDiv";
-counterDiv.textContent = "Flowers Clicked: 0";
+counterDiv.textContent = "Flower petals: 0";
 
 counterDiv.style.position = "absolute";
-counterDiv.style.top = "16px";
-counterDiv.style.left = "16px";
+counterDiv.style.top = "540px";
+counterDiv.style.left = "438px";
 counterDiv.style.padding = "6px 12px";
 counterDiv.style.borderRadius = "8px";
 counterDiv.style.fontWeight = "600";
@@ -44,30 +44,11 @@ flowerImg.style.cursor = "pointer";
 let clickCount = 0;
 flowerImg.addEventListener("click", () => {
   clickCount += 1;
-  counterDiv.textContent = `Flowers Clicked: ${clickCount}`;
+  counterDiv.textContent = `Flower petals: ${clickCount}`;
 
   flowerImg.classList.remove("bounce");
   void flowerImg.offsetWidth;
   flowerImg.classList.add("bounce");
 });
 
-const btn: HTMLButtonElement = document.createElement("button");
-btn.textContent = "🌸 Click Me";
-btn.style.marginTop = "20px";
-btn.style.padding = "6px 12px";
-btn.style.fontSize = "20px";
-btn.style.backgroundColor = "#fabbda";
-btn.style.color = "white";
-btn.style.border = "none";
-btn.style.borderRadius = "8px";
-btn.style.cursor = "pointer";
-btn.style.zIndex = "3";
-btn.addEventListener("click", () => {
-  flowerImg.classList.remove("bounce");
-  void flowerImg.offsetWidth;
-  flowerImg.classList.add("bounce");
-});
-
-container.append(glow, flowerImg, counterDiv, btn);
-
-console.log("hello");
+container.append(glow, flowerImg, counterDiv);
