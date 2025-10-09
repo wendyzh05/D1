9b14fd5334548c65f23c663b8b4eccdd09e9424c
Wendy Zhang <156264212+wendyzh05@users.noreply.github.com>
2025-10-09 03:11:04 +0000

    Added automatic clicking (Step 3 complete)

--- src/main.ts
+++ src/main.ts
@@ -19,19 +19,31 @@ document.body.append(container);
 const counterDiv: HTMLDivElement = document.createElement("div");
 counterDiv.id = "counterDiv";
 counterDiv.textContent = "Flower petals: 0";
-
 counterDiv.style.position = "absolute";
 counterDiv.style.top = "540px";
-counterDiv.style.left = "438px";
+counterDiv.style.left = "405px";
+counterDiv.style.fontSize = "25px";
 counterDiv.style.padding = "6px 12px";
 counterDiv.style.borderRadius = "8px";
 counterDiv.style.fontWeight = "600";
 counterDiv.style.fontFamily = "sans-serif";
-counterDiv.style.color = "#fff";
+counterDiv.style.color = "#000000ff";
 counterDiv.style.zIndex = "1000";
 counterDiv.style.background = "rgba(255, 182, 193, 0.5)";
 counterDiv.style.backdropFilter = "blur(4px)";
-counterDiv.style.boxShadow = "0 0 10px rgba(255, 182, 193, 0.4)";
+counterDiv.style.boxShadow = "0 0 10px rgba(233, 164, 197, 0.4)";
+
+const clickText = document.createElement("div");
+clickText.textContent = "Click 🌸";
+clickText.style.position = "absolute";
+clickText.style.marginTop = "10px";
+clickText.style.top = "150px";
+clickText.style.left = "475px";
+clickText.style.fontSize = "22px";
+clickText.style.fontWeight = "600";
+clickText.style.color = "#ffffffff";
+clickText.style.textShadow = "0 0 6px rgba(61, 47, 50, 0.6)";
+clickText.style.zIndex = "3";
 
 const flowerImg: HTMLImageElement = document.createElement("img");
 flowerImg.src = flowerImage;
@@ -51,4 +63,9 @@ flowerImg.addEventListener("click", () => {
   flowerImg.classList.add("bounce");
 });
 
-container.append(glow, flowerImg, counterDiv);
+setInterval(() => {
+  clickCount += 1;
+  counterDiv.textContent = `Flower petals: ${clickCount}`;
+}, 1000);
+
+container.append(glow, flowerImg, clickText, counterDiv);
